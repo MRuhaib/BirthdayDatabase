35f9ce616b24308bee81d9220889e4ee13957251
MRuhaib <115531547+MRuhaib@users.noreply.github.com>
2023-07-11 20:03:28 +0530

    Update BDDBbackend.js
    implemented getting closest birthday feature

--- BDDBbackend.js
+++ BDDBbackend.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 const express = require('express');
 const Birthday = require('./models/birthdays.js');
+const { locales } = require('moment/moment.js');
 
 
 const app = express();
@@ -98,5 +99,57 @@ app.post('/update', function(req, res){
                 })
             } 
         })
+    })
+})
+
+//to get the closest birthday:
+app.get('/closestBday', function(req, res){
+    res.render('closestBday', {statement : ''});
+});
+
+app.post('/closest', function(req, res){
+    Birthday.find()
+    .then(function(result){
+        //getting the specified person's bday and adding everyone else's to an array
+        var Name = req.body.Person;
+        var BDays = [];
+        var BDay1 = '';
+        result.forEach(function(element){
+            if(element.Person === Name){
+                BDay1 = new Date(element.Birthday);
+                console.log('birthday of the given guy is ' + element.Birthday)
+            }else{
+                BDays.push(element);
+            }
+        })
+        var closestBday = new Date(BDays[0].Birthday);
+        var closestName = '';
+        var diff = '';
+
+        //simple bubble sort to get the closest date :)
+        for(let i = 1; i< BDays.length; i++){
+            var Bday2 = new Date(BDays[i].Birthday);
+            var diff1 = Math.abs(BDay1 - closestBday)
+            var diff2 = Math.abs(BDay1 - Bday2)
+            if(diff2 < diff1){
+                closestBday = Bday2;
+                diff = Math.ceil(diff2/(1000 * 60 * 60 * 24))
+            }
+        }
+
+        //to get the date object in YYYY-MM-DD form
+        var year = closestBday.toLocaleString("default", { year: "numeric" });
+        var month = closestBday.toLocaleString("default", { month: "2-digit" });
+        var day = closestBday.toLocaleString("default", { day: "2-digit" });
+        var formattedDate = year + "-" + month + "-" + day;
+
+        //to get the corresponding name
+        BDays.forEach(function(element){
+            if(element.Birthday === formattedDate){
+                closestName = element.Person;
+            }
+        })
+        var statement = 'The closest birthday is ' + closestName + "'s on " + formattedDate + ", which is nearly " + diff +' days away.'
+        res.render('closestBday', {statement : statement});
     })
 })
